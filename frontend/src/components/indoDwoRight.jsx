b0e8e41dd52142f2c6b443b7a5cab7cc1b5dd132
namo333 <dad688634@gmail.com>
2023-10-02 15:25:32 +0300

    com

--- frontend/src/components/indoDwoRight.jsx
+++ frontend/src/components/indoDwoRight.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const IndoDwoRight = () => (
     <main>
         <section className='bg-[#F7F9FB] rounded-[40px] flex px-[56px] py-[42px] flex-wrap'>
-            <p>dfgdsghsfgh</p>
+            <div className="">
+                <h1 className='text-zinc-950 text-5xl leading-10 mb-0 pb-0'>Anonymous payments</h1>
+                <p className='text-neutral-900 text-opacity-80 text-xl font-normal leading-normal'>Use allow_anonymous parameterin <span className='text-[#41A4DF]'>createInvoice</span> method to give useran option to remain private</p>
+            </div>
         </section>
     </main>
 )
--- frontend/src/components/infoDwoLeft.jsx
+++ frontend/src/components/infoDwoLeft.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
+import Bodyphone from '../icons/Body.svg';
+
 const InfoDwoLeft = () => (
     <main>
         <section className='bg-[#F7F9FB] rounded-[40px] flex px-[56px] py-[42px] flex-wrap'>
-            <p>dfgdsghsfgh</p>
-            <p>fghdfgjhdfj</p>
-            <p>fghdfgjhdfj</p>
-            <p>fghdfgjhdfj</p>
-            <p>fghdfgjhdfj</p>
-            <p>fghdfgjhdfj</p>
-            <p>fghdfgjhdfj</p>
-            
+            <div className="">
+                <img src={Bodyphone} alt="" />
+            </div>
+
+            <div className="">
+                <h1 className='text-zinc-950 text-5xl leading-10 mb-0 pb-0'>Send coins to users</h1>
+                <p className='text-neutral-900 text-opacity-80 text-xl font-normal leading-normal'>Automate payouts to users with <span className='text-[#41A4DF]'>transfer</span> API method   </p>
+            </div>
         </section>
     </main>
 )
